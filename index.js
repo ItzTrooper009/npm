8c3a9cf4d0d6b264522a637c4137a1cd879e54b1
Ankit Singh Chouhan <ankitt335407@gmail.com>
2023-04-25 12:42:35 +0530

    npm Final commit

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ console.clear();
 const _ = require("underscore");
 
 console.log(_.contains([1, 2, 3], 2));
+
+/**
+ * npm i -> for installing all dependencies
+ * npm i <package name> -> to install a specific package
+ * npm i <package name>@<version> -> to install a specific version of a package
+ * npm i <package name> -D ->install a package as a development dependency
+ * npm-check-updates -> to check all the packages if they are updated or not
+ * ncu -u -> to force update the package.json file to latest versions
+ * npm i -> to update dependencies as  respected to the pakage.json
+ */
